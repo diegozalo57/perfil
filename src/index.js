7a3a2733809ea90aeeeaae7f443ca9d90c64784f
diegozalo57 <diegozalo57@gmail.com>
2021-02-03 13:22:26 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-import './assets/styles/Styles.scss'
-
-const container = document.getElementById('app');
-
-ReactDOM.render(<App />, container);
